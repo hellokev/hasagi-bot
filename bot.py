669a2ad6cf20218e9b5f88be4918629ba26f57fc
hellokev <2kevinloi8@gmail.com>
2023-11-08 03:10:05 -0800

    test

--- bot.py
+++ bot.py
@@ -1,10 +1,30 @@
 import discord
+from discord.ext import commands
 import os 
 from dotenv import load_dotenv
 
 load_dotenv()
 
 SECRET_TOKEN = os.getenv("TOKEN")
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='~', intents=intents)
+
+@bot.command(name='test')
+async def _test(ctx, arg):
+    pass
+
+client = MyClient(intents=intents)
+client.run(SECRET_TOKEN)
 
 def run_discord_bot():
     print(SECRET_TOKEN)
